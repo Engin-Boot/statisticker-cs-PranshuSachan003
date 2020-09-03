60d9747af25a5dbc4c5e5a1f843cec0b1ffdf9e0
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-09-03 10:43:53 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -6,6 +6,30 @@ namespace Statistics
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
+            if(numbers.Count==0)
+                throw.Error.ArgumentNull("numbers");
+            double sum = 0;
+            float average;
+            for(int i=0;i<numbers.count;i++)
+                sum=sum+numbers[i];
+            average = (float)(sum/numbers.count);
+            return average;
+            float min = numbers[0];
+            for(int i=1;i<numbers.count;i++)
+            {
+                if(numbers[i]<min)
+                    min=numbers[i];
+            }
+            return min;
+            float max = numbers[0];
+            for(int i=1;i<numbers.count;i++)
+            {
+                if(numbers[i]>max)
+                    max=numbers[i];
+            }
+            return max;
+            if (double.IsNaN(average)||double.IsNaN(min)||double.IsNaN(mx))
+            throw new ArithmeticException();
             //Implement statistics here
         }
     }
