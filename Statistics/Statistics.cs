a749ed39fea01093336dd8462a65309b395c0eac
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-09-03 12:56:33 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,29 +3,37 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public class Stats
+    {
+        float average;
+        float min;
+        float max;
+    }
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
-            double sum = 0;
-            float average;
+            double sumOfNumbers = 0;
+            float averageOfNumbers;
             for(int i=0;i<numbers.count;i++)
-                sum=sum+numbers[i];
-            average = (float)(sum/numbers.count);
-            return average;
-            float min = numbers[0];
+                sumOfNumbers=sumOfNumbers+numbers[i];
+            averageOfNumbers = (float)(sumOfNumbers/numbers.count);
+            float minOfNumbers = numbers[0];
             for(int i=1;i<numbers.count;i++)
             {
-                if(numbers[i]<min)
-                    min=numbers[i];
+                if(numbers[i]<minOfNumbers)
+                    minOfNumbers=numbers[i];
             }
-            return min;
-            float max = numbers[0];
+            float maxOfNumbers = numbers[0];
             for(int i=1;i<numbers.count;i++)
             {
-                if(numbers[i]>max)
-                    max=numbers[i];
+                if(numbers[i]>maxOfNumbers)
+                    maxOfNumbers=numbers[i];
             }
-            return max;
+            Stats obj = new Stats();
+            obj.average = averageOfNumbers;
+            obj.min = minOfNumbers;
+            obj.max = maxOfNumbers;
+            return obj;
             if (double.IsNaN(average)||double.IsNaN(min)||double.IsNaN(mx))
             throw new ArithmeticException();
             //Implement statistics here
